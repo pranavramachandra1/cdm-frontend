21613e3d6c3aea99ce83fc2145fa3da005465a56
pranavramachandra1 <pranavramachandra@berkeley.edu>
2025-07-27 17:58:32 -0400

    next.config change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-      domains: ['http://localhost:3000'],
+      domains: ['http://localhost:3000', 'lh3.googleusercontent.com'],
     },
 };
 
